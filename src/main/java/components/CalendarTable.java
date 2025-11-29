d08a5bd63010fb13d40733034f62e4efb6dbbc4e
BotIzer <izerboti@gmail.com>
2025-11-29 13:39:20 +0100

    Added navigation to week view

--- src/main/java/components/CalendarTable.java
+++ src/main/java/components/CalendarTable.java
@@ -5,6 +5,7 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Insets;
+import java.time.LocalDateTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -20,12 +22,13 @@ import javax.swing.SwingConstants;
 
 import main.java.models.Model;
 import main.java.models.Task;
+import main.java.views.WindowBase;
 import main.java.views.WindowBase.View;
 
 
 
 public class CalendarTable extends JPanel{
-    
+    protected LocalDateTime showDate = Model.now;
     protected View view = View.WEEK;
     
     public CalendarTable(View v){
@@ -35,8 +38,10 @@ public class CalendarTable extends JPanel{
         if (v == View.YEAR) buildYear();
     }
     private void buildWeek(){
-        this.setLayout(new GridLayout(1, 7));
-        int date = Model.now.getDayOfYear(); 
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        JPanel container = new JPanel(new GridLayout(1, 7));
+        int date = showDate.getDayOfYear(); 
         for (int i = date - 3; i < date + 4; i++) {
             ArrayList<Task> dayList = new ArrayList<>();
             for (Task task : Model.tasks) {
@@ -44,12 +49,62 @@ public class CalendarTable extends JPanel{
                     dayList.add(task);
                 }
             }
-            String day = Model.now.plusDays((long)i-date).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
-            String dateString = Model.now.plusDays((long)i-date).format(DateTimeFormatter.ofPattern(Model.getDateFormat()));
+            String day = showDate.plusDays((long)i-date).getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+            String dateString = showDate.plusDays((long)i-date).format(DateTimeFormatter.ofPattern(Model.getDateFormat()));
             DayContainer dc = new DayContainer(day, dateString, dayList, View.WEEK);
-            this.add(dc);
+            container.add(dc);
         }
+        JPanel navigation = new JPanel(new GridLayout(1, 3));
+        JButton prev = new JButton("<");
+        JButton res = new JButton("Reset");
+        JButton next = new JButton(">");
+        navigation.setBackground(Model.BackGroundColor);
+        prev.setBackground(Model.PrimaryColor);
+        prev.setForeground(Model.TextColor);
+        next.setBackground(Model.PrimaryColor);
+        next.setForeground(Model.TextColor);
+        res.setBackground(Model.PrimaryColor);
+        res.setForeground(Model.TextColor);
+        prev.setFocusable(false);
+        next.setFocusable(false);
+        res.setFocusable(false);
+
+        prev.addActionListener(e -> changeWeek(-1));
+        res.addActionListener(e -> changeWeek(0));
+        next.addActionListener(e -> changeWeek(1));
+
+        navigation.add(prev);
+        navigation.add(res);
+        navigation.add(next);
+
+        gc.fill = GridBagConstraints.BOTH;
+        gc.gridy = 0;
+        gc.weightx = 1;
+        this.add(navigation, gc);
+        gc.gridy = 1;
+        gc.weighty = 1;
+        this.add(container, gc);
     }
+
+    public void changeWeek(int direction){
+        this.removeAll();
+        switch (direction) {
+            case 0:
+                showDate = Model.now;
+                break;
+            case 1:
+                showDate = showDate.plusDays(7);
+                break;
+            case -1:
+                showDate = showDate.minusDays(7);
+                break;
+            default:
+                showDate = Model.now;
+                break;
+        }
+        WindowBase.getInstance().refresh();
+    }
+
     private void buildMonth(){
         this.setLayout(new GridLayout(5, 7));
         for (int i = 0; i < 31; i++) {
@@ -118,4 +173,5 @@ public class CalendarTable extends JPanel{
         this.repaint();
         this.revalidate();
     }
+
 }
